336d359e045ac75e7ce72d4c0c4ff88dd86cfe79
Bagaric <joseph@josephit.com>
2018-11-08 10:24:02 +0100

    Add babel transpiling

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-class EacCounter {
-  static async getTotalEthTransferred() {
-    const baseUrl = `http://api.etherscan.io/api?module=account&action=txlist&startblock=0&endblock=99999999&sort=asc`;
-    const timestampSchedulerUrl = `${baseUrl}&address=0x09e0c54ed4cffca45d691d5eb7b976d650f5904c`;
-    const blockSchedulerUrl = `${baseUrl}&address=0x56efae8a6d07fb29c24e67d76f3eccac180cf527`;
-
-    const urls = [timestampSchedulerUrl, blockSchedulerUrl];
-
-    let promises = [];
-
-    urls.forEach(url => {
-      const resultPromise = fetch(url).then(async (resp) => {
-        const response = await resp.json();
-
-        if (response.status == "1" && response.message === "OK") {
-          const weiTransferred = response.result.reduce((acc, tx) => acc + parseInt(tx.value), 0);
-          return weiTransferred / 1e18;
-        } else {
-          throw Error(response.result);
-        }
-      });
-      promises.push(resultPromise);
-    });
-
-    const values = await Promise.all(promises);
-    const totalEthTransferred = values.reduce((acc, value) => acc + parseFloat(value), 0);
-
-    return totalEthTransferred;
-  }
-}
+var EacCounter = require('./dist/EacCounter');
 
 module.exports = EacCounter;
